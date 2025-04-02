809887d8592a3cf5e54ccaa6b659ecff7cd268fe
andres.mera <feli041089@gmail.com>
2025-04-02 14:42:50 -0500

    code 01: Project creation.

---
+++ src/main/java/com/project/app_dog_back/AppDogBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.app_dog_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDogBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDogBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/app_dog_back/AppDogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.app_dog_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDogBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
