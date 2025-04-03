51763e4d2b274805825aa1d953b892fa0eef5d68
andres.mera <feli041089@gmail.com>
2025-04-03 14:51:36 -0500

    code 02: Adding a new service for mood query by ID.

--- src/main/java/com/project/app_dog_back/application/service/IAnimoServiceImpl.java
+++ src/main/java/com/project/app_dog_back/application/service/IAnimoServiceImpl.java
@@ -2,25 +2,41 @@ package com.project.app_dog_back.application.service;
 
 import com.project.app_dog_back.application.dto.AnimoDto;
 import com.project.app_dog_back.application.mapper.IAnimoMapper;
+import com.project.app_dog_back.domain.model.catalog.Animo;
 import com.project.app_dog_back.domain.repository.IAnimoRepository;
 import com.project.app_dog_back.domain.service.IAnimoService;
+import com.project.app_dog_back.domain.service.IMessageService;
+import com.project.app_dog_back.insfraestructure.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class IAnimoServiceImpl implements IAnimoService {
 
     private IAnimoRepository iAnimoRepository;
+    private IMessageService iMessageService;
 
     @Autowired
-    public IAnimoServiceImpl(IAnimoRepository iAnimoRepository) {
+    public IAnimoServiceImpl(IAnimoRepository iAnimoRepository, IMessageService iMessageService) {
         this.iAnimoRepository = iAnimoRepository;
+        this.iMessageService = iMessageService;
     }
 
     @Override
     public List<AnimoDto> getAll() {
         return iAnimoRepository.findAll().stream().map(IAnimoMapper.INSTANCE::toDto).toList();
+    }
+
+    @Override
+    public Animo getById(Long idAnimo) {
+        Locale locale = LocaleContextHolder.getLocale();
+        String mensaje = iMessageService.getMensaje("war.repeated", locale);
+        return iAnimoRepository.findById(idAnimo)
+                .orElseThrow(() -> new NotFoundException(mensaje, String.valueOf(HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND));
     }
 }
--- src/main/java/com/project/app_dog_back/domain/service/IAnimoService.java
+++ src/main/java/com/project/app_dog_back/domain/service/IAnimoService.java
@@ -1,9 +1,12 @@
 package com.project.app_dog_back.domain.service;
 
 import com.project.app_dog_back.application.dto.AnimoDto;
+import com.project.app_dog_back.domain.model.catalog.Animo;
 
 import java.util.List;
 
 public interface IAnimoService {
     List<AnimoDto> getAll();
+
+    Animo getById(Long idAnimo);
 }
