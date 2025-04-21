29a584c96c41319500c46a4fa2d9259090b00935
andres.mera <feli041089@gmail.com>
2025-04-21 07:44:02 -0500

    code 09: Mood catalog documentation. - Part 05.

--- src/main/java/com/project/app_dog_back/domain/service/IAnimoService.java
+++ src/main/java/com/project/app_dog_back/domain/service/IAnimoService.java
@@ -1,6 +1,6 @@
 package com.project.app_dog_back.domain.service;
 
-import com.project.app_dog_back.application.dto.AnimoDto;
+import com.project.app_dog_back.application.dto.catalog.AnimoDto;
 import com.project.app_dog_back.domain.model.catalog.Animo;
 
 import java.util.List;
