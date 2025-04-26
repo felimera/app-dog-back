b3e5b58acee266e3454f9d42fabafc1fe35c1abf
andres.mera <feli041089@gmail.com>
2025-04-26 10:32:25 -0500

    code 10: Body Condition Index catalog documentation.

--- src/main/java/com/project/app_dog_back/application/mapper/IIccMapper.java
+++ src/main/java/com/project/app_dog_back/application/mapper/IIccMapper.java
@@ -1,6 +1,6 @@
 package com.project.app_dog_back.application.mapper;
 
-import com.project.app_dog_back.application.dto.IccDto;
+import com.project.app_dog_back.application.dto.catalog.IccDto;
 import com.project.app_dog_back.domain.model.catalog.Icc;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
